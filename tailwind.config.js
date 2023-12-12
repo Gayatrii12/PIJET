dabbed0ae8adf4e66ae899e9d18b997a59dc5829
Ayush-Gala <ayushgala2@gmail.com>
2023-12-12 12:37:54 +0530

    Completed Ethics and Policies Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'pijet-blue': '#354f77'
+      }
+    },
   },
   plugins: [],
 }
