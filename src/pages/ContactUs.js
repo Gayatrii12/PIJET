224510ed03a96dd9dbe8da810f8768d0359dd732
Ayush-Gala <ayushgala2@gmail.com>
2024-01-29 13:38:51 +0530

    ISSN changes part infinity

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -9,21 +9,25 @@ const ContactUs = () => {
     <div className="max-w-7xl md:mx-auto p-6 my-4 text-justify ring-4 ring-pijet-blue mx-auto w-11/12 md:w-3/5 ">
       
       <div className='flex text-lg md:text-xl mb-4'>
-        <div className='w-1/2 px-1 md:px-6 text-pijet-blue'>
+        <div className='w-2/5 px-1 md:px-6 text-pijet-blue'>
           <strong> Issuing/Publishing Body:</strong>
         </div>
-        <div>
+        <div className='text-left'>
+          <p>SCTR's Pune Institute of Computer Technology</p>
+          <p>Survey No-27, Near Trimurti Chowk,</p>
+          <p>Dhankawadi, Pune-411043 (India)</p>
+          <p>Phone No: 24371101 (Ext 104)</p>
           <p>Dr. S. T. Gandhe</p>
           <p>Principal, PICT, Pune.</p>
           <p><a className='text-blue-500' href="mailto:principal@pict.edu">principal@pict.edu</a></p>
-          <p>020-24371101 Ext:104</p>
+          {/* <p>020-24371101 Ext:104</p> */}
         </div>
       </div>
       <div className='flex text-lg md:text-xl mb-4'>
-        <div className='w-1/2 px-1 md:px-6 text-pijet-blue'>
+        <div className='w-2/5 px-1 md:px-6 text-pijet-blue'>
           <strong>Coordinator:</strong>
         </div>
-        <div>
+        <div className='text-left'>
           <p>Dr. R. C. Jaiswal</p>
           <p>PIJET Journal Coordinator</p>
           <p><a className='text-blue-500' href="mailto:pijet@pict.edu">pijet@pict.edu</a></p>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
     return (
         <div className="max-w-7xl mx-auto p-6 text-justify">
-        
+
 
             <div className="md:flex py-4 ">
                 <div className="w-full md:w-1/2  md:pr-4 ">
@@ -16,7 +16,7 @@ const HomePage = () => {
                         <p className="text-2xl text-pijet-blue pb-2 ">
                             <strong>Journal Particulars:</strong>
                         </p>
-                        
+
                         {/* <li><strong>ISSN Number:</strong> ABCD-EFGH</li>
                         <li><strong>Impact Factor:</strong> 6.3</li>
                         <li><strong>Acceptance Rate:</strong> 12%</li> */}
@@ -24,16 +24,24 @@ const HomePage = () => {
                         <li><strong>Open Access Publication</strong></li>
                         <li><strong>Peer Reviewed Journal</strong></li>
                         <li><strong>Bi-Annually Published Issues</strong></li>
-                        <li><strong>Issuing/Publishing Body:</strong> <br/> &emsp; Dr. S. T. Gandhe 
-                        <br/>&emsp; Principal, PICT, Pune.
-                        <br/>&emsp; <a className='text-blue-500' href="mailto:principal@pict.edu">principal@pict.edu</a>
+                        <li><strong>Issuing/Publishing Body:</strong>
+                            <div className='px-2'>
+                                &emsp; SCTR's Pune Institute of Computer Technology
+                                <br />&emsp; Survey No-27, Near Trimurti Chowk,
+                                <br />&emsp; Dhankawadi, Pune-411043 (India)
+                                <br />&emsp; Phone No: 24371101 (Ext 104)
+                                <br />&emsp; Dr. S. T. Gandhe
+                                <br />&emsp; Principal, PICT, Pune.
+                                <br />&emsp; <a className='text-blue-500' href="mailto:principal@pict.edu">principal@pict.edu</a>
+                            </div>
                         </li>
+
                         <li><strong>Institution/Organization: </strong> <Link to="https://www.pict.edu" target="_blank" className="text-blue-500">SCTR's Pune Institute of Computer Technology</Link></li>
                         <li><strong>Current Issue:</strong> Volume 1 - Issue 1 (<Link to="/archives" className="text-blue-500">View in Archives</Link>)</li>
                         <li><strong>Language:</strong> English</li>
                         <br />
                         <p><strong>Important Links and Forms:</strong></p>
-                        <br/>
+                        <br />
                         <div className="text-s justify-evenly">
                             <li><a href="/PIJET_COPYRIGHT.pdf" className="text-blue-500">Copyright Form</a></li>
                             <li><Link to="/register" className="text-blue-500">Manuscript Submission Form</Link></li>
@@ -57,9 +65,9 @@ const HomePage = () => {
                 <h1 className="text-3xl md:text-4xl font-bold mb-4 text-pijet-blue py-3">About SCTR's Pune Institute of Computer Technology:</h1>
 
                 <p className="mb-2 text-xl">
-                Nestled in the heart of Pune- the “Oxford of the East”, a city known for its rich cultural heritage and dynamic tech industry, lies  <Link to="https://www.pict.edu">SCTR's Pune Institute of Computer Technology</Link>. With a laser-sharp focus on academic excellence, research, and innovation, PICT has earned a reputation as one of India's premier institutions for computer education, attracting a diverse and driven student body from around the country. PICT stands as a beacon of academic excellence, propelled by a vision to be the leader in higher technical education and research of international repute. At the heart of our mission is the commitment to becoming the most sought-after institute for education and research in emerging engineering and technology disciplines. PICT's research potential is underscored by its status as a recognized research center by Savitribai Phule Pune University (SPPU) and its continuous engagement in diverse research projects funded by prestigious organizations like DST, AICTE, DRDO, and RGSTC. 
-                <br /><br />
-                Founded in 1983 by visionary stalwarts- Shri. Gopal Krishna, Shri. N. Ramakrishna, and Shri. R. S. Kothavale- PICT has been at the forefront of computer education and research for almost four decades. PICT's recognition in national rankings and accreditation, coupled with its dedicated faculty, places it at the forefront of technological education and research, making it an ideal destination for those aspiring to contribute to cutting-edge advancements in computer science and technology.
+                    Nestled in the heart of Pune- the “Oxford of the East”, a city known for its rich cultural heritage and dynamic tech industry, lies  <Link to="https://www.pict.edu">SCTR's Pune Institute of Computer Technology</Link>. With a laser-sharp focus on academic excellence, research, and innovation, PICT has earned a reputation as one of India's premier institutions for computer education, attracting a diverse and driven student body from around the country. PICT stands as a beacon of academic excellence, propelled by a vision to be the leader in higher technical education and research of international repute. At the heart of our mission is the commitment to becoming the most sought-after institute for education and research in emerging engineering and technology disciplines. PICT's research potential is underscored by its status as a recognized research center by Savitribai Phule Pune University (SPPU) and its continuous engagement in diverse research projects funded by prestigious organizations like DST, AICTE, DRDO, and RGSTC.
+                    <br /><br />
+                    Founded in 1983 by visionary stalwarts- Shri. Gopal Krishna, Shri. N. Ramakrishna, and Shri. R. S. Kothavale- PICT has been at the forefront of computer education and research for almost four decades. PICT's recognition in national rankings and accreditation, coupled with its dedicated faculty, places it at the forefront of technological education and research, making it an ideal destination for those aspiring to contribute to cutting-edge advancements in computer science and technology.
                 </p>
             </div>
 
