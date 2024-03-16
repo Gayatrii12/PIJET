7b12cc93075c2f4e7bc0a73bbd8507f8d6e62537
asp21k <atharva.pardeshi0023@gmail.com>
2024-03-16 12:21:37 +0530

    added developers page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-
-import './App.css';
+import "./App.css";
 
 import { ThemeProvider } from "@material-tailwind/react";
-import EthicsAndPolicies from './pages/EthicsAndPolicies';
-import HomePage from './pages/HomePage';
-import Guidelines from './pages/Guidelines';
-import ContactUs from './pages/ContactUs';
-import Navbar from './components/navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Ourteam from './pages/our-team';
-import Archive from './pages/Archive';
-import CallForPapers from './pages/CallForPapers';
-import Register from './pages/Register';
-import NotFound from './pages/NotFound';
+import EthicsAndPolicies from "./pages/EthicsAndPolicies";
+import HomePage from "./pages/HomePage";
+import Guidelines from "./pages/Guidelines";
+import ContactUs from "./pages/ContactUs";
+import Navbar from "./components/navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Ourteam from "./pages/our-team";
+import Archive from "./pages/Archive";
+import CallForPapers from "./pages/CallForPapers";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import Footer from "./components/footer";
+import Developers from "./pages/Developers";
 function App() {
   return (
-
     <div className="App">
       <ThemeProvider>
         <BrowserRouter>
@@ -30,10 +30,11 @@ function App() {
             <Route path="/contact-us" element={<ContactUs />} />
             <Route path="/register" element={<Register />} />
             <Route path="*" element={<NotFound />} />
+            <Route path="/developers" element={<Developers />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
       </ThemeProvider>
-
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className="flex flex-col justify-center items-center text-center p-3 bg-pijet-blue">
+      <h1 className=" text-[#ffffff] font-semibold flex">
+        © 2023-2024 All rights reserved. | Created by &nbsp;
+        <Link
+          to="/developers"
+          className="text-white font-semibold duration-500 ease-in-out cursor-pointer underline underline-offset-2  "
+        >
+          <p className="hover:scale-110 transition-transform ease-in-out text-white">
+            PIJET Tech Team
+          </p>
+        </Link>
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
