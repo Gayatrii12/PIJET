8494a5c40acef9dc847151471ac9a0d34ec2a175
asp21k <atharva.pardeshi0023@gmail.com>
2024-04-21 16:08:46 +0530

    added list

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -5,9 +5,7 @@ import { Vol1_Issue1_paperMap } from "../data/paperMaps";
 import PaperMap from "../components/PaperMap";
 
 const Archive = () => {
-  // const volumes = [];
-  const keys = Object.keys(Vol1_Issue1_paperMap);
-
+  const data = Object.entries(Vol1_Issue1_paperMap);
 
   return (
     <div className="max-w-7xl mx-auto p-6 text-justify">
@@ -17,25 +15,22 @@ const Archive = () => {
 
       <h1 className="text-3xl font-bold mt-6 text-center text-pijet-blue">
         Present Issue
-      </h1>
+      </h1> 
       <h1 className="text-2xl font-bold my-6 text-center text-pijet-blue">
         Complete Volume 1 Issue 1 : Dec 2023
       </h1>
-      <div className="grid grid-cols-3 gap-2">
-        {keys.map((key, index) => (
-          <Link
-            key={index}
-            to={Vol1_Issue1_paperMap[key]}
-            className="w-full h-full"
-          >
-            <button
+      <div className="flex flex-col items-center justify-center w-2/5 border-2 bg-slate-100 ">
+      {data.map(([key, value], index) => (
+          <a key={index} href={value} className="w-full h-full">
+            <p
               key={index}
-              className="text-black text-lg font-sans text-center py-1 border-2 bg-slate-100 hover:bg-slate-200"
+              className="text-black text-lg h- w-full font-sans text-center hover:bg-slate-200 py-1 "
             >
-              {key}
-            </button>
-          </Link>
+              {key.substring(0, 50)}
+            </p>
+          </a>
         ))}
+       
       </div>
     </div>
   );
